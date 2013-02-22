4793486df86e2245a4de58afab7cfe8dd517eb16
Steffen Sørensen <steffen@sublife.dk>
2013-02-22 11:04:11 +0100

    Added small function overloads, for handling both string and chars

--- lib/DateConversion.cpp
+++ lib/DateConversion.cpp
@@ -19,24 +19,33 @@ bool logcollect::DateConversion::addFormat(std::string* format){
 	return true;
 }
 
+bool logcollect::DateConversion::addFormat(const char* format){
+	std::string* str_format = new std::string(format);
+	return this->addFormat(str_format);
+}
+
 time_t logcollect::DateConversion::getTime(const std::string* str_time){
+	return this->getTime(str_time->c_str());
+}
+
+time_t logcollect::DateConversion::getTime(const char* str_time){
 	std::vector<const std::string*>::iterator it;
 	time_t detected_time = time(NULL);
 	
 	tm* result = new tm();
 	bool found_format = false;
 	
-	// std::cout << *str_time << std::endl;
+	std::cout << str_time << " " << this->formats.size() << std::endl;
 	
 	for (it = this->formats.begin() ; it != this->formats.end(); it++){
-		char* first = strptime(str_time->c_str(), (*it)->c_str(), result);
+		char* first = strptime(str_time, (*it)->c_str(), result);
 		
-		// std::cout << "Hit: " << first << std::endl;
+		std::cout << "Hit: " << (*it)->c_str() << std::endl;
 		if(first != NULL){
-			// std::cout << "converted: " << (*it)->c_str() << std::endl;
+			std::cout << "converted: " << (*it)->c_str() << std::endl;
 			found_format = true;
 			detected_time = timegm(result);
-			// std::cout << "time: " << detected_time << std::endl;
+			std::cout << "time: " << detected_time << std::endl;
 			break;
 		}
 	}
